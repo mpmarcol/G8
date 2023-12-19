7a58985a6ac737e650a58f14c06725d6e2f98138
mpmarcol <mpmarcol@gmail.com>
2023-12-18 22:25:46 -0300

    1218d item.js done

--- js/item.js
+++ js/item.js
@@ -1,11 +1,23 @@
-const add = document.querySelector('#add');
-const subtract = document.querySelector('#subtract');
+const add = document.querySelector('#item-add');
+const subtract = document.querySelector('#item-rest');
 const quantity = document.querySelector('#quantity');
+
+// console.log(add, subtract, quantity);
 
 add.addEventListener('click', () => quantity.value = Number(quantity.value) + 1);
 subtract.addEventListener('click', () => {
     quantity.value = Number(quantity.value) === 0
     ? 0
-    : Number(quantity.value) - 1
+    : Number(quantity.value) - 1;
+    
+    quantity.value = Number(quantity.value) < 0
+    ? 0
+    : Number(quantity.value)+0;
 });
-quantity.addEventListener('change', () => quantity.value = Number(quantity.value) < 0);
+quantity.addEventListener('change', () => {
+    quantity.value = Number(quantity.value) < 0
+    ? 0
+    : Number(quantity.value)+0;
+});
+
+
